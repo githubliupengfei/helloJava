d2f8355c9b8e0b57235cae3c417748f5877f5748
liupengfei <pengfei.liu@hipac.cn>
2016-10-18 18:05:40 +0800

    test

---
+++ src/com/hello/test01/TestDemo01.java
@@ -0,0 +1,12 @@
+package com.hello.test01;
+
+/**
+ * Created by liupengfei on 16/10/18.
+ */
+public class TestDemo01 {
+    public static void  main (String args[]){
+        System.out.println("Hello！！！");
+
+    }
+
+}
